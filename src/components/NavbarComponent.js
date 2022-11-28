29a857fa06e9ade9358810a096f17a27f248d9d9
Rama Chandra Viswanadh <ramachandraviswanadh333@gmail.com>
2022-11-28 12:38:44 +0530

    photogallery app complete

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,34 +1,42 @@
-import React from 'react'
-import {Container,Nav, Navbar} from 'react-bootstrap';
 
-const NavbarComponent =()=> {
-  return (
+
+import { Container, Navbar, Nav } from "react-bootstrap"
+
+import { NavLink } from 'react-router-dom'
+
+const NavbarComponent = () => {
+	return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="#home">
-      <img
-              alt="logo"
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtHPKP8BnK-fyWsW5P2SujCDlwwQUlXHhIzPhLo4nC7Fha8AlFQwKjcukHlf8FPRTM-lM&usqp=CAU"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{'  '}Photo Gallery App
-            </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="">Home</Nav.Link>
-          
-         
-        </Nav>
-        <Nav>
-          <Nav.Link href="">About Us</Nav.Link>
-          <Nav.Link href="">Contact Us</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
-  )
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
+          <img
+            alt="logo"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtHPKP8BnK-fyWsW5P2SujCDlwwQUlXHhIzPhLo4nC7Fha8AlFQwKjcukHlf8FPRTM-lM&usqp=CAU"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Photo Gallery App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={NavLink} to="/about">
+              About Us
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact Us
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavbarComponent
